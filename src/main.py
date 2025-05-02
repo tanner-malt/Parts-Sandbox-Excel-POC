4ad5ffbc4c820ff64321e9391a79231ee949dfdf
Tanner <tannerohio@gmail.com>
2025-05-02 09:11:35 -0400

    removed a print statement, not needed

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import GUI
 import manager
 
 if __name__ == "__main__":
-    print("Starting Parts Sandbox Manager", flush=True)
     app_manager = manager.ApplicationManager()
     GUI.start_application()
 
